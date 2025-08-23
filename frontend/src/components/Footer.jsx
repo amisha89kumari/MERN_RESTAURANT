d93f13932dc922c15ead90c685b414f1037d3aaa
Amisha Kumari <amishakumari4487@gmail.com>
2025-08-23 18:19:24 +0530

    Updated Footer and Navbar

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="banner">
-          <div className="left">ZEESHU</div>
+          <div className="left">Saffron Cafe </div>
           <div className="right">
-            <p>Z6 Gulshan-e-Maymar, Karachi</p>
+            <p>Saffron Cafe , Bistupur</p>
             <p>Open: 05:00 PM - 12:00 AM</p>
           </div>
         </div>
